5bd124757824d4370f7bb8d6d7cfe3394a4480a2
Zsolt Kovcs <zs.myth@gmail.com>
2012-02-25 17:54:53 +0100

    initial commit

---
+++ src/main/groovy/org/beedom/dslforge/demo/SeleniumDelegate.groovy
@@ -0,0 +1,27 @@
+package org.beedom.dslforge.demo
+
+import com.thoughtworks.selenium.DefaultSelenium;
+
+class SeleniumDelegate {
+    
+    @Delegate
+    private DefaultSelenium selenium
+    
+    public SeleniumDelegate() {
+        selenium = new DefaultSelenium("localhost", 4444, context.browser, context.url)
+    }
+    
+    public SeleniumDelegate(String browser, String url) {
+        selenium = new DefaultSelenium("localhost", 4444, browser, url)
+    }
+    
+    def static dslKey = "seleniumD"
+
+    //Stupid eclipse issue force this empty implementation
+    def void open(String s1, String s2) {
+    }
+    //Stupid eclipse issue force this empty implementation
+    def String getLog() {
+        retrun null
+    }
+}
